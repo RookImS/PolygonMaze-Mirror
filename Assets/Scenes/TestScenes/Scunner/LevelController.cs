e0f3aa2f09c34810a10bc023b064fa31b035126b
skdiakffn <skdiakffn@ajou.ac.kr>
2020-09-18 04:01:22 +0900

    test unitiy NavMesh

---
+++ Assets/Scenes/TestScenes/Scunner/LevelController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public Transform prefab;
+    public void newObject()
+    {
+        Object.Instantiate(prefab, new Vector3(3.73f, 0.5f, 6.5f), Quaternion.identity);
+    }
+}
