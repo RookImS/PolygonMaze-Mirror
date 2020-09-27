8fa9675a68fcc526624e0c5b3661eda2035c14ee
skdiakffn <skdiakffn@ajou.ac.kr>
2020-09-28 04:24:10 +0900

    modift wall prefab

--- Assets/Scripts/StageComponet/DestinationBehaviour.cs
+++ Assets/Scripts/StageComponet/DestinationBehaviour.cs
@@ -1,12 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 
-
-public class LevelEditor : MonoBehaviour
+public class DestinationBehaviour : MonoBehaviour
 {
 
 }
-
-
