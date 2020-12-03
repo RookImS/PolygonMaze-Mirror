5931e35ceb70277212fe2ca50bd166c7999f59f6
yachung <dbstjd8267@ajou.ac.kr>
2020-12-03 22:59:44 +0900

    Modify tutorialUI, UI Layer
    Former-commit-id: ef07a043b3485c2dacae4911e2bace421dd2028d

--- Assets/Scripts/Tutorial/Tutorial.cs
+++ Assets/Scripts/Tutorial/Tutorial.cs
@@ -6,17 +6,19 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     public GameObject aniUI;
+    public GameObject maskUI;
+    //public GameObject maskObject;
+    public GameObject mask;
     public GameObject fieldAniUI;
     public TutorialObject tutorial;
     public DialogueUI dialogueUI;
-
+    
     private List<TutorialObject.Tutorial> tutorialList;
     private TutorialChecker tutorialChecker;
     private int phase;          // 튜토리얼 중 몇 번째 챕터인가를 나타냄
     private int phaseLength;    // 튜토리얼이 몇 챕터로 이루어져있는지 나타냄
     private int chapterLength;  // 튜토리얼 현재 챕터의 길이
     private int chapterOrder;   // 튜토리얼 현재 챕터내에서 몇 번째 동작을 하는 중인지 나타냄
-
     public event Action nextTutorialChapter;
 
     private void Awake()
@@ -36,7 +38,6 @@ public class Tutorial : MonoBehaviour
         phaseLength = 0;
         chapterLength = 0;
         chapterOrder = 0;
-
         tutorialList = new List<TutorialObject.Tutorial>();
         foreach (TutorialObject.Tutorial temp in tutorial.tutorialList)
             tutorialList.Add(new TutorialObject.Tutorial(temp));
@@ -74,8 +75,10 @@ public class Tutorial : MonoBehaviour
                     {
                         ani.enable = true;
                         GameObject tempAniObject = Instantiate(ani.obj, aniUI.transform);
-                        tempAniObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(ani.posX, ani.posY);
-                        tempAniObject.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0f, 0f, ani.rotation);
+                        //tempAniObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(ani.posX, ani.posY);
+                        //tempAniObject.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0f, 0f, ani.rotation);
+                        tempAniObject.transform.position = new Vector3(ani.posX, 10, ani.posY);
+                        tempAniObject.transform.localRotation = Quaternion.Euler(90f, 0f, ani.rotation);
                         StartCoroutine(DestroyAniObject(tempAniObject, chapterOrder + ani.length));
                     }
                 }
@@ -95,6 +98,39 @@ public class Tutorial : MonoBehaviour
                 }
             }
 
+            //tutorialList[phase].maskAnimationList.obj = mask;
+
+            //if (!tutorialList[phase].maskAnimation.enable)
+            //{
+            //    if (tutorialList[phase].maskAnimation.order == chapterOrder)
+            //    {
+            //        maskUI.SetActive(true);
+            //        tutorialList[phase].maskAnimation.enable = true;
+            //        tutorialList[phase].maskAnimation.obj.transform.localPosition = new Vector2(tutorialList[phase].maskAnimation.posX, tutorialList[phase].maskAnimation.posY);
+            //        tutorialList[phase].maskAnimation.obj.transform.localRotation = Quaternion.Euler(0f, 0f, tutorialList[phase].maskAnimation.rotation);
+            //        //tutorialList[phase].maskAnimation.obj.SetActive(false);
+            //    }
+            //    else
+            //        maskUI.SetActive(false);
+            //}
+            if(tutorialList[phase].maskAnimationList.Count == 0)
+            {
+                maskUI.SetActive(false);
+            }
+            foreach (AniObject ani in tutorialList[phase].maskAnimationList)
+            {
+                if (!ani.enable)
+                {
+                    if (ani.order == chapterOrder)
+                    {
+                        maskUI.SetActive(true);
+                        ani.enable = true;
+                        ani.obj = mask;
+                        ani.obj.transform.localPosition = new Vector2(ani.posX, ani.posY);
+                        ani.obj.transform.localRotation = Quaternion.Euler(0f, 0f, ani.rotation);
+                    }
+                }
+            }
         }
     }
 
